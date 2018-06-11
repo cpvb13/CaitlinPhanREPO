e3533e72a2b1d7742d1c21343a87ef58e05f7162
cpvb13 <cait@berkeley.edu>
2018-06-10 23:58:06 -0700

    Tip calculator in python

---
+++ week2_tip.py
@@ -0,0 +1,8 @@
+price_str = input("Enter the price of a meal: ")
+price = float(price_str)
+
+tip = price * 0.16
+total = price + tip
+
+print("A 16% tip would be", tip)
+print("The total including the tip would be", total)
