4bd85f10a3b3224bf44d358a6007fb3636b9e844
cpvb13 <40078770+cpvb13@users.noreply.github.com>
2018-06-13 14:21:28 -0700

    Update todo.py
    finished to do list

--- todo.py
+++ todo.py
@@ -15,15 +15,29 @@ User can type "get" and the program will ask for the day and print the values
 User can type "quit" to exit the loop
 '''
 a = 'You have to'
-to_do = {"You have to ": 'Monday','You have to ': 'Tuesday','You have to ': 'Wednesday','You have to ': 'Thursday','You have to ': 'Friday','You have to ': 'Saturday','You have to ': 'Sunday'}
+to_do = {'Monday':"You have to ", 'Tuesday':"You have to ", 'Wednesday':"You have to ", 'Thursday':"You have to ", 'Friday':"You have to ", 'Saturday':"You have to ", 'Sunday':"You have to "}
 #day = key when calling to_do[key]
-print(to_do[1])
+print(to_do['Monday'])
 
 input1 = input("What would you like to do?")
 if input1 == 'add':
     input2 =input('What day?')
     day = input2
-    input3 = input("What would you like to add to " +day +"'s to-do list?")    
+    input3 = input("What would you like to add to " +day +"'s to-do list?")
+    to_do['Monday']+= input3
+    input1 = input("Okay, what would you like to do now?")
+    
+if input1 == 'get':
+    input2a = input('What day?')
+    if to_do[str(input2a)] == "You have to ":
+        to_do[str(input2a)]+= "do nothing"
+
+       # input2a = 'nothing'
+    print(to_do[str(input2a)]+" on "+input2a+'.')
+    input1 = input("Now, what would you like to do?")
+
+if input1 == 'quit' or 'Quit' or 'q':
+    print('Okay, bye!')
 else:
     print("Sorry, I cannot do that.")
 #to_do
