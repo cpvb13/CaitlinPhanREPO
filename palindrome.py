b8a28cc33d6c6ab62c480303233eb88be5cc5c09
cpvb13 <40078770+cpvb13@users.noreply.github.com>
2018-06-13 11:38:06 -0700

    Create palindrome.py
    Finished palindrome

---
+++ palindrome.py
@@ -0,0 +1,26 @@
+inpu = input("Enter your name: ")
+
+#inpu = 'John'
+#inpu = "Caitlin Phan"
+#name = list(name)
+#inpu = 'Racecar'
+name = inpu.split(' ')
+words = len(name)
+palindrome = []
+
+
+j=0
+while j<words:
+    palindrome.append(name[j][::-1])
+    j+=1
+    
+name_b = ' '.join(palindrome)
+name_b = name_b.title()
+print("Olleh "+name_b+'!')
+
+if name_b == inpu:
+    result ='a palindrome!'
+else:
+    result ="not a palindrome! :("
+
+print('Your name is '+result)
