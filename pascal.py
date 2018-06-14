57df3f321e1a18a963cd18ca2d18cdcd47346036
cpvb13 <cait@berkeley.edu>
2018-06-13 21:10:46 -0700

    started traingle

---
+++ pascal.py
@@ -0,0 +1,37 @@
+n = int(input("Enter a number: "))
+
+tri=[[1],[1,1]]
+
+if n >= len(tri):
+    for i in range(n):
+    row=[]
+    row.append()
+    tri.append(row)
+
+for i in
+'''
+if n >= len(tri):
+    a = int(tri(0))
+    row=[]
+    row.append(a)
+
+
+
+if n == 1:
+    print(tri)
+if n == 2:
+    print(int('1 1'))
+
+while n > 2:
+    print('1')
+    print()
+    print('1')
+    row = int(n)
+    print(row)
+    n -= 1
+
+
+while n > 0:
+    print(1)
+    n -= 1
+'''
